5b828dc0ae743510a82babc3f1117128b3c44af6
SemihKahveci <hasansemihkahveci@gmail.com>
2025-11-10 21:59:09 +0300

    next implementation for website completed.

--- Andron/next.config.ts
+++ Andron/next.config.ts
@@ -3,6 +3,8 @@ const nextConfig = {
   output: 'standalone',
   // basePath sadece production'da aktif olsun
   basePath: process.env.NODE_ENV === 'production' ? '/home' : '',
+  // assetPrefix basePath ile aynı olmalı (production'da)
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/home' : '',
 };
 
 export default nextConfig;
