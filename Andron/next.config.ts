8966fb29d5e196038f4a3b31ae6f0a42b555c0e7
SemihKahveci <hasansemihkahveci@gmail.com>
2026-02-05 23:28:42 +0300

    next config updated.

--- Andron/next.config.ts
+++ Andron/next.config.ts
@@ -2,7 +2,10 @@
 const nextConfig = {
   poweredByHeader: false,
   output: 'standalone',
-
+// basePath sadece production'da aktif olsun
+basePath: process.env.NODE_ENV === 'production' ? '/home' : '',
+// assetPrefix basePath ile aynı olmalı (production'da)
+assetPrefix: process.env.NODE_ENV === 'production' ? '/home' : '',
   images: {
     remotePatterns: [
       {
